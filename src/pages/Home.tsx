c373c7c79329c40abec80d023d42b618e315c829
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2023-07-27 08:06:23 -0300

    add helpers dir experimenting with quotes api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
+// import getApi from '../helpers/fetchQuote';
+
+// const quoteData = getApi('https://zenquotes.io/api/random/');
+
 function Home() {
-  return <h1>Hello!</h1>;
+  return (
+    <>
+      <h1>Hello!</h1>
+      {/* <div>{quoteData}</div> */}
+    </>
+  );
 }
 
 export default Home;
