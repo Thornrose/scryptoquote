04bee04af65b4d2f818ef5f082071f0d4cad83c2
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2023-09-27 09:57:42 -0300

    set up newline for author name

--- src/pages/LetterList.tsx
+++ src/pages/LetterList.tsx
@@ -4,7 +4,8 @@ import { v4 as uuidv4 } from 'uuid';
 import LetterListItem from './LetterListItem';
 import PunctuationListItem from './PunctuationListItem';
 
-const regexPunctuation: RegExp = / |'|,|\.|&|-|\n/g;
+const regexPunctuation: RegExp = / |'|,|\.|&|-|!/g;
+const regexNewline: RegExp = /\n/g;
 
 type PuzzleArrayProps = {
   puzzle: string[];
@@ -14,14 +15,17 @@ export default function LetterList(props: PuzzleArrayProps) {
   const { puzzle } = props;
 
   const letters = puzzle.map((letter) => {
-    if (!letter.match(regexPunctuation)) {
-      return <LetterListItem key={uuidv4()} quoteLetter={letter} />;
+    if (letter.match(regexPunctuation)) {
+      return <PunctuationListItem key={uuidv4()} quoteLetter={letter} />;
     }
-    return <PunctuationListItem key={uuidv4()} quoteLetter={letter} />;
+    if (letter.match(regexNewline)) {
+      return <div key={uuidv4()} className="w-full" />;
+    }
+    return <LetterListItem key={uuidv4()} quoteLetter={letter} />;
   });
 
   return (
-    <ul className="flex flex-row flex-wrap justify-center gap-x-2 p-8 font-pangolin text-4xl">
+    <ul className="flex flex-row flex-wrap justify-center gap-x-2 p-4 font-pangolin text-4xl">
       {letters}
     </ul>
   );
